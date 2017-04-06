bb4a456aa1a889bf22c7f1f4045dbbb54b17ebc2
DonChef660 <fehmi@raqipi.ch>
2017-04-06 16:56:43 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,4 +4,5 @@ if (!defined('TYPO3_MODE')) {
 }
 
 // Add new fields to the list of fields which should be overlaid on page records:
-$TYPO3_CONF_VARS['FE']['pageOverlayFields'] .= ',tx_pagesaddfields_customtext';
+$TYPO3_CONF_VARS['FE']['pageOverlayFields'] .= ',tx_pagesaddfields_customtitle';
+$TYPO3_CONF_VARS['FE']['pageOverlayFields'] .= ',tx_pagesaddfields_customsubtitle';
